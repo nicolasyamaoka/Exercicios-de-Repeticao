cc82004a577b1a40ca2ef9e284ff99207571c6c8
nicolasyamaoka <nicolas.yamaoka@unifesp.br>
2026-04-08 11:47:43 -0300

    Update ex46.c

--- ex46.c
+++ ex46.c
@@ -5,13 +5,13 @@ int main() {
 	printf("\nDigite dois valores em sequencia");
 	scanf("\n%f",&x);
 
-	while(i<1){
+	do{
 		scanf("\n%f",&y);
 		if(y == 0){
 			printf("\n Valor Inválido");
 		} else {
 			i++;
-		}
+		}while(i<1)
 	}
 	resultado = x / y;
 	printf("\no resultado da divisão do primeiro valor lido pelo segundo é %f",resultado);
