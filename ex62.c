1018145aeca774c8a2101aa628c2dac89fad5451
nicolasyamaoka <nicolas.yamaoka@unifesp.br>
2026-04-08 20:42:05 -0300

    Create ex62.c

---
+++ ex62.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    float notas, media;
+    int alunos, i, total = 0;
+    printf("Digite quantos alunos existem em determinada turma: ");
+    scanf("%d", &alunos);
+
+    for(i = 1; i <= alunos; i++){
+        printf("\nDigite a nota do Aluno %d: ", i);
+        scanf("%f", &notas);
+        total = total + notas;
+        printf("%d", alunos);
+    }
+    media = total / alunos;
+    printf("\no valor da media das notas dos alunos e %f", media);
+}
