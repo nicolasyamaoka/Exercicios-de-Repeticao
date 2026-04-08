5e44b079dcf460dba9ac0b60fc0b9e19d932bd9d
nicolasyamaoka <nicolas.yamaoka@unifesp.br>
2026-04-08 20:50:29 -0300

    Create ex64.c

---
+++ ex64.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    float valor, total;
+    int i; 
+
+    for(i = 1; i <= 10; i++){
+        printf("Digite um valor: ");
+        scanf("%f", &valor);
+
+        if (valor < 40){
+        total = total + valor;
+    }
+    }
+    printf("\na soma dos 10 numeros e %f", total);   
+}
